fe3bd7903e748d3bdaa39494a9ce8614cb5866b2
Anubhav Kumar Singh <aanusingh927@gmail.com>
2026-02-23 22:30:27 +0530

    Update numberguess.py

--- numberguess.py
+++ numberguess.py
@@ -14,7 +14,6 @@ print("Player2, start guessing!")
 attempts=0
 is_coreect=False
 
-#This loop runs until Player2 guesses correctly while not is_correct
 guess=float(input("Enter your Guess:"))
 attempts+=1
 if guess==target:
@@ -24,5 +23,6 @@ elif guess>target:
  print("Try a lower number:")
 else:
  print("Try a higher number")      
+
 
 start_game()
