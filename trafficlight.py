dc283fe3b19ff23a0be9c173afc654e6b6a9d1e3
Anubhav Kumar Singh <aanusingh927@gmail.com>
2026-03-18 15:13:29 +0530

    Add files via upload
    In this I have practice about if and elif condition in Python

---
+++ trafficlight.py
@@ -0,0 +1,10 @@
+light = input("Enter The TRAFFIC LIGHT Color (RED, YELLOW , GREEN):")
+
+if light == "RED":
+    print("stop")
+elif light == "YELLOW" :
+    print("slow" ) 
+elif light == "GREEN":
+    print("go")
+else:
+    print("Invalid Input: Please Enter COrrect Light Color")
